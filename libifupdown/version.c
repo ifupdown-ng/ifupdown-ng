3734aaecbd13d6743bfaae760bb27e1a0332f520
Ariadne Conill <ariadne@dereferenced.org>
2020-12-04 00:26:01 -0700

    version: use a single printf() call for the entire about text, saves 104 bytes on aarch64

--- libifupdown/version.c
+++ libifupdown/version.c
@@ -21,20 +21,21 @@
 void
 lif_common_version(void)
 {
-	printf("%s %s\n", PACKAGE_NAME, PACKAGE_VERSION);
-
-	printf("\nCopyright (c) 2020 Ariadne Conill <ariadne@dereferenced.org>\n");
-	printf("Copyright (c) 2020 Maximilian Wilhelm <max@sdn.clinic>\n\n");
-
-	printf("Permission to use, copy, modify, and/or distribute this software for any\n");
-	printf("purpose with or without fee is hereby granted, provided that the above\n");
-	printf("copyright notice and this permission notice appear in all copies.\n\n");
-
-	printf("This software is provided 'as is' and without any warranty, express or\n");
-	printf("implied.  In no event shall the authors be liable for any damages arising\n");
-	printf("from the use of this software.\n\n");
-
-	printf("Report bugs at <%s>.\n", PACKAGE_BUGREPORT);
+	printf("%s %s\n"
+	       "\n"
+	       "Copyright (c) 2020 Ariadne Conill <ariadne@dereferenced.org>\n"
+	       "Copyright (c) 2020 Maximilian Wilhelm <max@sdn.clinic>\n"
+	       "\n"
+	       "Permission to use, copy, modify, and/or distribute this software for any\n"
+	       "purpose with or without fee is hereby granted, provided that the above\n"
+	       "copyright notice and this permission notice appear in all copies.\n"
+	       "\n"
+	       "This software is provided 'as is' and without any warranty, express or\n"
+	       "implied.  In no event shall the authors be liable for any damages arising\n"
+	       "from the use of this software.\n"
+	       "\n"
+	       "Report bugs at <%s>.\n",
+	       PACKAGE_NAME, PACKAGE_VERSION, PACKAGE_BUGREPORT);
 
 	exit(EXIT_SUCCESS);
 }
